52d2007aebf5d6f600b36e07c19648655f8307c1
Varma-14 <115480985+Varma-14@users.noreply.github.com>
2022-10-10 20:56:47 +0530

    Add files via upload
    week1

---
+++ week1/day1/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels= 2;
+	System.out.println(noOfWheels);
+	short noOfMirrors= 2;
+	System.out.println(noOfMirrors);
+	long chassisNumber= 22987665889763L;
+	System.out.println(chassisNumber);
+	boolean isPuntured= false;
+	System.out.println(isPuntured);
+	String bikeName= "Duke";
+	System.out.println(bikeName);
+	double runningKM= 45.888;
+	System.out.println(runningKM);
+	
+	
+	
+}
+}
