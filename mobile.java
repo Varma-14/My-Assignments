ff291fffe46a6a39b405611b37e0be38bb6959b7
Varma-14 <115480985+Varma-14@users.noreply.github.com>
2022-10-10 21:40:55 +0530

    Add files via upload
    week1

---
+++ mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mobileBrandName= "iPhone 14";
+System.out.println(mobileBrandName);
+char mobileLogo=   'i';
+System.out.println(mobileLogo);
+short noOfMobilePiece= 100;
+System.out.println(noOfMobilePiece);
+int mobileNumber= 2022;
+System.out.println(mobileNumber);
+long mobileImeiNumber= 860417254506608685L;
+System.out.println(mobileImeiNumber);
+float mobilePrice= 189900.9991F;
+System.out.println(mobilePrice);
+boolean isDamaged= false;
+System.out.println(isDamaged);
+
+
+
+	}
+
+}
