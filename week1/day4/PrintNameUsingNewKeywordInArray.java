295ec155f154827c48fd9c7d503dcb9492e8bf65
Varma-14 <115480985+Varma-14@users.noreply.github.com>
2022-10-13 23:02:18 +0530

    Add files via upload

---
+++ week1/day4/PrintNameUsingNewKeywordInArray.java
@@ -0,0 +1,19 @@
+package week1.day4;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrintNameUsingNewKeywordInArray {
+	public static void main(String[] args) {
+		String[] name=new String[5];
+		name[0]= "Azhgu";
+		name[1]= "Nithil";
+		name[2]= "zubair";
+		name[3]="prasaanth";
+		name[4]= "Nadeem";
+		System.out.println(Arrays.toString(name));
+				
+	}
+
+}
